774df6b15067566323613d3e2132f05583d6c45d
faizalrawas <61645764+faizalrawas@users.noreply.github.com>
2023-01-04 10:50:06 +0300

    Add files via upload

---
+++ fainal/wam_tch.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:00:07 12/29/2022 
+-- Design Name: 
+-- Module Name:    wam_tch - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity wam_tch is
+end wam_tch;
+
+architecture Behavioral of wam_tch is
+
+begin
+
+
+end Behavioral;
+
